f78626f8532e788fa40dcb710f95cf3255534014
Ravindra Kumar <ravidsrk@gmail.com>
2016-08-06 15:29:09 +0530

    Syntax error fixed

--- generators/app/index.js
+++ generators/app/index.js
@@ -19,15 +19,19 @@ module.exports = yeoman.Base.extend({
       store: true,
       default: this.appname,
       validate: function (input) {
-        if (/^([a-zA-Z0-9_]*)$/.test(input)) return true;
-        return 'Your application name cannot contain special characters or a blank space, using the default name instead : ' + defaultAppBaseName;
+        if (/^([a-zA-Z0-9_]*)$/.test(input)) {
+          return true;
+        }
+        return 'Your application name cannot contain special characters or a blank space, using the default name instead : ' + this.appname;
       }
     },
       {
         name: 'package',
         message: 'What package will you be publishing the app under?',
         validate: function (input) {
-          if (/^([a-z_]{1}[a-z0-9_]*(\.[a-z_]{1}[a-z0-9_]*)*)$/.test(input)) return true;
+          if (/^([a-z_]{1}[a-z0-9_]*(\.[a-z_]{1}[a-z0-9_]*)*)$/.test(input)) {
+            return true;
+          }
           return 'The package name you have provided is not a valid Java package name.';
         },
         default: 'in.boilerplate.sample',
