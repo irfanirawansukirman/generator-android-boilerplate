28a7e5ebfa7bbea7f7858d1beb02358aeeec273b
Ravindra Kumar <ravidsrk@gmail.com>
2016-08-06 12:33:10 +0530

    Added script to update the repo

---
+++ repo_update/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const renamer = require('renamer');
+const rimraf = require('rimraf');
+const path = require('path');
+const Git = require("nodegit");
+const replace = require("replace");
+const filter = require('filter-files');
+const findInFiles = require('find-in-files');
+
+// Clone a given repository into the `./tmp` folder.
+rimraf.sync(__dirname + '/templates')
+
+Git.Clone("https://github.com/ribot/android-boilerplate", "../generators/app/templates")
+  // Look up this known commit.
+  .then(function(repo) {
+    console.log("Completed cloning");
+
+    replace({
+      regex: "uk.co.ribot.androidboilerplate",
+      replacement: "<%= appPackage %>",
+      paths: ['../generators/app/templates'],
+      recursive: true,
+      silent: false,
+    });
+
+
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
